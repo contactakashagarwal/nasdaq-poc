37e3ceab7113cb6a37a8c56f5b3e58730c340dd6
contactakashagarwal@gmail.com <contactakashagarwal@gmail.com>
2021-10-18 13:49:04 +0530

    tracking pageview events

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,8 +5,27 @@ import "../components/main/mainpage.css";
 import "../components/main/directors-grid/directors-grid.css";
 import "../components/main/body-content/body-content.css";
 import "../components/footer/footer.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
