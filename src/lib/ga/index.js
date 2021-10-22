e4a5924dca2cc1c3c4daab67f50ff33ecbbb9653
contactakashagarwal@gmail.com <contactakashagarwal@gmail.com>
2021-10-22 16:37:27 +0530

    rr

--- src/lib/ga/index.js
+++ src/lib/ga/index.js
@@ -1,20 +1,22 @@
 // log the pageview with their URL
-export const pageview = (url) => {
-  window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-    page_path: url,
-  });
-};
+// export const pageview = (url) => {
+//   window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+//     page_path: url,
+//   });
+// };
 
 // log specific events happening.
 export const event = ({ action, params }) => {
   if ("ga" in window) {
-    tracker = ga.getAll()[0];
-    if (tracker) tracker.send("event", action, params);
+    let tracker = window.ga.getAll()[0];
+    if (tracker) {
+      tracker.send("event", action, params);
+    }
   }
 
   // window.ga("event", action, params);
 };
 
-export const eventUsingGa = ({ category, action }) => {
-  window.ga("send", "event", category, action);
-};
+// export const eventUsingGa = ({ category, action }) => {
+//   window.ga("send", "event", category, action);
+// };
