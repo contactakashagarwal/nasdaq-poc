897586ee9d97df8159e8034f3b4c843b4d877ce9
contactakashagarwal@gmail.com <contactakashagarwal@gmail.com>
2021-10-23 23:35:29 +0530

    pointing  to prod gtm

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=InOK-T5m8dFccj38Zsr3cQ&gtm_preview=env-8&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=W0aXpkWa9AVPOzJ99Bhvww&gtm_preview=env-9&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PS357RD');
             `,
             }}
